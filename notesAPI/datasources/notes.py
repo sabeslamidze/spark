1874f34b5ca069779069b839099ed9ecda2f3bc8
Safiko Abeslamidze <sofiko.abeslamidze@gmail.com>
2021-10-07 00:56:07 +0200

    Introduce NotesContoller

---
+++ notesAPI/datasources/notes.py
@@ -0,0 +1,27 @@
+import uuid
+import datetime
+
+class NotesManager:
+    def __init__(self, storage):
+        self.storage = storage
+    
+    def getAllNotes(self):
+        return list(self.storage.values())
+    
+    def getNote(self, noteId):
+        if note := self.storage.get(noteId):
+            return note
+        raise Exception(f"Note with id {noteId} is not found!")
+
+    def postNote(self, content):
+        if not content:
+            raise Exception("No content found!")
+        key = str(uuid.uuid4())
+        time = datetime.datetime.utcnow().isoformat(sep='T', timespec='milliseconds') + 'Z'
+        newNote = {"id": key, "created": time, "content": content}
+        return self.storage.setdefault(key, newNote)
+    
+    def deleteNote(self, noteId):
+        if noteId not in self.storage:
+            raise Exception(f"Note with id {noteId} is not found!")
+        del self.storage[noteId]
---
+++ notesAPI/views.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request, jsonify
+from .datasources import NotesManager
+
+notes = Blueprint("notes", __name__)
+
+storage = {} 
+manager = NotesManager(storage)
+
+@notes.route("/note")
+def getAllNotes():
+    try:
+        return jsonify(manager.getAllNotes()), 200
+    except Exception as e:
+        return str(e), 500
+
+@notes.route("/note/<string:noteId>")
+def getNote(noteId):
+    try:
+        return jsonify(manager.getNote(noteId))
+    except Exception as e:
+        return str(e), 500
+
+@notes.route("/note", methods=["POST"])
+def postNote():
+    try:
+        content = request.json.get("content")
+        return jsonify(manager.postNote(content)), 200
+    except Exception as e:
+        return str(e), 500
+
+@notes.route("/note/<string:noteId>", methods=["DELETE"])
+def deleteNote(noteId):
+    try:
+        manager.deleteNote(noteId)
+        return "OK", 200
+    except Exception as e:
+        return str(e), 500 
